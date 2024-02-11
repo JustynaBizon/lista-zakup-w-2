8cce3683a97e513d25b6120bed67497ce1f54705
Mateusz Rybka <mateusz.rybka2@ibm.com>
2024-02-11 20:09:15 +0100

    Final version

--- shoppingList.js
+++ shoppingList.js
@@ -5,15 +5,20 @@ const getData = async (url, method, elementId, onSuccess) => {
 
     const data = await response.json()
 
-    if (response.status === 200) {
-        onSuccess(data, elementId)
-    } else {
-        console.erorr('Bad request')
+    if (Array.isArray(data)) {
+        if (response.status === 200) {
+            onSuccess(data, elementId)
+        } else {
+            console.error('ERROR')
+        }
     }
 }
 
+const deleteProduct = async (productId) => {
+    await fetch(`http://localhost:3000/products/${productId}`, { method: 'DELETE' })
+}
+
 const asTable = (data, tableId) => {
-    tableId.get
     data.forEach((product) => {
         const tableRow = document.createElement('tr')
 
@@ -38,62 +43,49 @@ const asTable = (data, tableId) => {
         deleteButton.onclick = () => deleteProduct(product.id)
         deleteField.appendChild(deleteButton)
         tableRow.appendChild(deleteField)
-        
+
         tableId.appendChild(tableRow)
     })
 }
 
-const deleteProduct = async (productId) => {
-    await fetch(`http://localhost:3000/products/${productId}`, {method: 'DELETE'})
-    getData('http://localhost:3000/products', 'GET', shoppingTable, asTable)
+const sendRequest = async (url, method, body, onSuccess) => {
+    const response = await fetch(url, {
+        method: method,
+        body: JSON.stringify(body)
+    })
+
+    const data = await response.json()
+
+    if (response.status === 201) {
+        onSuccess(data)
+    } else {
+        console.error('ERROR')
+    }
 }
+
+// ----------------------------------------------------------------------------------
 
 const shoppingTable = document.getElementById('shoppingTable');
-
-shoppingTable.getElementsByTagName('tr').forEach
-
 getData('http://localhost:3000/products', 'GET', shoppingTable, asTable)
 
+const newProductForm = document.getElementById('new-product-form');
 
+newProductForm.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-function addProduct() {
-    const select = document.getElementById("productSelect");
-    const table = document.getElementById("shoppingTable");
-    const selectedOption = select.options[select.selectedIndex];
-    const productInfo = selectedOption.text.split(" - ");
+    const shopingTable = document.getElementById('shoppingTable')
+    const id = JSON.stringify(shopingTable.rows.length)
 
-    if(productInfo.length > 1) {
-        const row = table.insertRow(-1);
-        const cellProduct = row.insertCell(0);
-        const cellPrice = row.insertCell(1);
-        cellProduct.innerHTML = productInfo[0];
-        cellPrice.innerHTML = productInfo[1];
+    const nameInput = document.getElementById('name-input')
+    const priceInput = document.getElementById('price-input')
+
+    if (nameInput.value !== '' && priceInput.value !== '') {
+        sendRequest(
+            'http://localhost:3000/products',
+            'POST',
+            { id: id, name: nameInput.value, price: priceInput.value },
+            (data) => {
+                console.log(data);
+            })
     }
-}
-
-function removeProduct() {
-    const table = document.getElementById("shoppingTable");
-    if (table.rows.length > 1) {
-        table.deleteRow(-1);
-    }
-}
-
-function addNewProductToList() {
-    const newProductInput = document.getElementById("newProduct");
-    const productPriceInput = document.getElementById("productPrice");
-    const newProductName = newProductInput.value.trim();
-    const productPrice = productPriceInput.value.trim();
-
-    if (newProductName !== "" && productPrice !== "") {
-        const select = document.getElementById("productSelect");
-        const newOption = document.createElement("option");
-        newOption.value = newProductName;
-        newOption.text = `${newProductName} - ${productPrice} zł`;
-        select.appendChild(newOption);
-
-        newProductInput.value = "";
-        productPriceInput.value = "";
-    } else {
-        alert("Proszę wpisać nazwę produktu i cenę.");
-    }
-}
+})
